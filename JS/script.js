23a2b723735ae9bf8562abd9ed8c620befa8cffc
MatteoTuveri <matteo.tuveri16@gmail.com>
2023-10-23 19:56:16 +0200

    milestone 1

--- JS/script.js
+++ JS/script.js
@@ -16,35 +16,46 @@
 //creo gli oggetti con i dati a disposizione
 const memberList = [
     {
-        name:'Wayne Barnett',
-        role:'Founder & CEO',
-        picture:'wayne-barnett-founder-ceo.jpg'
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        picture: 'wayne-barnett-founder-ceo.jpg'
     },
     {
-        name:'Angela Caroll',
-        role:'Chief Editor',
-        picture:'angela-caroll-chief-editor.jpg'
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        picture: 'angela-caroll-chief-editor.jpg'
     },
     {
-        name:'Walter Gordon',
-        role:'Office Manager',
-        picture:'walter-gordon-office-manager.jpg'
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        picture: 'walter-gordon-office-manager.jpg'
     },
     {
-        name:'Angela Lopez',
-        role:'Social Media Manager',
-        picture:'angela-lopez-social-media-manager.jpg'
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        picture: 'angela-lopez-social-media-manager.jpg'
     },
     {
-        name:'Scott Estrada',
-        role:'Developer',
-        picture:'scott-estrada-developer.jpg'
+        name: 'Scott Estrada',
+        role: 'Developer',
+        picture: 'scott-estrada-developer.jpg'
     },
     {
-        name:'Barbara Ramos',
-        role:'Graphic Designer',
-        picture:'barbara-ramos-graphic-designer.jpg'
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        picture: 'barbara-ramos-graphic-designer.jpg'
     },
 ]
+
+// stampo in console in successione tutti i dati in ordine
+
+for (let i = 0; i < memberList.length; i++) {
+
+    console.group('card');
+    console.log(memberList[i].name);
+    console.log(memberList[i].role);
+    console.log(memberList[i].picture);
+    console.groupEnd();
+}
 
 
